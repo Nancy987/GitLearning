5ede68cf653f72c7a56e33d78e0ab836b506b924
tech200 <nancymangla199@gmail.com>
2022-05-31 23:56:01 +0530

    [Add] Identifying valid or invalid triangle Program

--- src/Q5Triangle.java
+++ src/Q5Triangle.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Q5Triangle {
+import java.util.Scanner;
+
+public class Q5Triangle {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a1 = scn.nextInt();
+        int a2 = scn.nextInt();
+        int a3 = scn.nextInt();
+
+        int sum = a1 + a2 + a3;
+
+        if(sum==180 && a1!=0 && a2!=0 && a3!=0){
+            System.out.println("Valid Triangle");
+        }else{
+            System.out.println("Invalid Triangle");
+        }
+    }
 }
