fe9c52c57ff5d132dce50c32053a00e0598b4b8d
tech200 <nancymangla199@gmail.com>
2022-06-09 16:20:45 +0530

    [Add] Program to find highest scoring word.

--- src/HighestScoringWord.java
+++ src/HighestScoringWord.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class HighestScoringWord {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HighestScoringWord {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a string containing words");
+        String str = scn.nextLine();
+        String maxWord = "";
+        int max = 0;
+
+        for (String word : str.split(" ")){
+            int sum = 0;
+            for(int i=0; i<word.length(); i++) {
+                char c = word.charAt(i);
+                if (c != ' ') {
+                    sum += c - 'a' + 1;
+                }
+            }
+            if(max<sum){
+                max = sum;
+                maxWord = word;
+            }
+        }
+        System.out.println(maxWord);
+    }
 }
